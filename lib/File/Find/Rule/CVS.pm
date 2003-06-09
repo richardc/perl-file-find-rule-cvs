49f92c88c40df44302cbcce302eeb543a8f687a6
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-06-09 15:48:28 +0000

    first new cvs rule?
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/File-Find-Rule-CVS@1217 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/File/Find/Rule/CVS.pm
+++ lib/File/Find/Rule/CVS.pm
@@ -1,6 +1,10 @@
 use strict;
 package File::Find::Rule::CVS;
-our $VERSION = '0.01';
+use File::Find::Rule;
+use Parse::CVSEntries;
+use base 'File::Find::Rule';
+use vars qw( $VERSION );
+$VERSION = '0.01';
 
 =head1 NAME
 
@@ -18,6 +22,34 @@ the contents CVS/Entries files.
 
 =cut
 
+sub File::Find::Rule::_cvs_entry {
+    my $self = shift;
+    my ($file, $path) = @_;
+
+    return $self->{_entries}{ $path }{ $file }
+      if exists $self->{_entries}{ $path };
+
+    my $parse = Parse::CVSEntries->new( "$path/CVS/Entries" )
+      or return;
+
+    $self->{_entries}{ $path } = { map { $_->name => $_ } $parse->entries };
+    return $self->{_entries}{ $path }{ $file };
+}
+
+sub File::Find::Rule::cvs_modified () {
+    my $self = shift()->_force_object;
+    my $sub = sub {
+        my $entry = $self->_cvs_entry( @_ ) or return;
+        return (stat $_)[9] > $entry->mtime;
+    };
+    $self->exec( $sub );
+}
+
 1;
 __END__
 
+=head1 SEE ALSO
+
+L<Parse::CVSEntries>, L<File::Find::Rule>
+
+=cut
